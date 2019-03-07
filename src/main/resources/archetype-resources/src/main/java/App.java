e247f8dc2f46842f8c61cf0954da77d0c8ef2cea
sunlaud <sunlaud@mail.ru>
2019-03-07 15:19:15 +0200

    Added maven-dependency plugin in build lifecycle (was not enabled before for some reason)

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,7 +1,13 @@
 package $groupId;
 
+import com.google.common.base.CaseFormat;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( uncamelize("helloWorld") );
+    }
+
+    static String uncamelize(String str) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, str).replace('-', ' ');
     }
 }
--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,5 +1,20 @@
 package $groupId;
 
-public class AppTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+
+    @Test
+    void someTest() {
+        //GIVEN
+        String src = "someFooBar";
+
+        //WHEN
+        String uncamelized = App.uncamelize(src);
+
+        //THEN
+        assertThat(uncamelized).isEqualTo("some foo bar");
+    }
 }
